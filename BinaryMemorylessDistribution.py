910f11db8ebf85465f762aff2de9f7398522aa48
Ido Tal <idotal@gmail.com>
2021-02-18 12:59:19 +0200

    The polar toolbox

--- BinaryMemorylessDistribution.py
+++ BinaryMemorylessDistribution.py
@@ -1,3 +1,5 @@
+import math
+
 class BinaryMemorylessDistribution:
     def __init__(self):
         self.probs = []  # probs[yindex][xindex]
@@ -9,5 +11,23 @@ class BinaryMemorylessDistribution:
             errorProbSum += min(probPair)
 
         return errorProbSum
+
+    def bhattacharyya(self):
+        bhattacharyyaSum = 0.0
+
+        for probPair in self.probs:
+            bhattacharyyaSum += math.sqrt(probPair[0]*probPair[1])
+
+        return 2.0 * bhattacharyyaSum
+
+    def totalVariationDistance(self):
+        tvSum = 0.0
+
+        for probPair in self.probs:
+            tvSum += abs(probPair[0] - probPair[1])
+
+        return tvSum
+
+
 
 
--- test.py
+++ test.py
@@ -4,8 +4,10 @@ bmd = BinaryMemorylessDistribution.BinaryMemorylessDistribution()
 
 p = 0.1
 
-bmd.probs.append( [p, 1-p] )
-bmd.probs.append( [1-p, p] )
+bmd.probs.append( [0.5*p, 0.5*(1-p)] )
+bmd.probs.append( [0.5*(1-p), 0.5*p] )
 
 print( bmd.errorProb() )
+print( bmd.bhattacharyya() )
+print( bmd.totalVariationDistance() )
 

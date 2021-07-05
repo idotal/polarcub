1c48ff415806f983817a98307cd7bedf1639c2f0
Ido Tal <idotal@gmail.com>
2021-07-05 13:36:08 +0300

    We can simulate a channel

--- test3.py
+++ test3.py
@@ -1,22 +1,49 @@
 #! /usr/bin/env python3
 
 import BinaryTrellis
+import random
 
-bt = BinaryTrellis.BinaryTrellis(2)
+def deletionChannelSimulation(codeword, p, seed):
+    N = len(codeword)
+    receivedWord = []
 
-# print(bt)
+    random.seed(seed)
 
-fromVertex_stateId = 3
-fromVertex_verticalPosInLayer = 17
-fromVertex_layer = 0
 
-toVertex_stateId = 4
-toVertex_verticalPosInLayer = 14
-toVertex_layer = 1
+    for i in range(N):
+        r = random.random()
 
-edgeLabel = 0
-edgeProb = 0.5
+        if r < p:
+            pass
+        else:
+            receivedWord.append(codeword[i])
 
-bt.addToEdgeProb(fromVertex_stateId, fromVertex_verticalPosInLayer, fromVertex_layer, toVertex_stateId, toVertex_verticalPosInLayer, toVertex_layer, edgeLabel, edgeProb)
+    return receivedWord
 
-print(bt)
+def temp():
+    bt = BinaryTrellis.BinaryTrellis(2)
+    
+    # print(bt)
+    
+    fromVertex_stateId = 3
+    fromVertex_verticalPosInLayer = 17
+    fromVertex_layer = 0
+    
+    toVertex_stateId = 4
+    toVertex_verticalPosInLayer = 14
+    toVertex_layer = 1
+    
+    edgeLabel = 0
+    edgeProb = 0.5
+    
+    bt.addToEdgeProb(fromVertex_stateId, fromVertex_verticalPosInLayer, fromVertex_layer, toVertex_stateId, toVertex_verticalPosInLayer, toVertex_layer, edgeLabel, edgeProb)
+    
+    print(bt)
+
+codeword = [1,0,1,1,1,0]
+p = 0.9
+seed = 0
+
+receivedWord = deletionChannelSimulation(codeword, p, seed)
+print(codeword)
+print(receivedWord)

da2db0a46d62351d9c1dd75dd87eb2cccb869e2f
Ido Tal <idotal@gmail.com>
2021-07-12 19:01:33 +0300

    More housekeeping

--- BinaryMemorylessDistribution.py
+++ BinaryMemorylessDistribution.py
@@ -597,3 +597,66 @@ def upgradedLeftRightProbs(dataLeft, dataCenter, dataRight):
 
     return probMergeLeft, probMergeRight 
 
+def calcFrozenSet_degradingUpgrading(n, L, upperBoundOnErrorProbability, xDistribution, xyDistribution):
+    """Calculate the frozen set by degrading and upgrading the a-priori and joint distributions, respectively
+
+    Args:
+        n (int): number of polar transforms
+
+        L (int): number of quantization levels, for both encoding and decoding
+
+        upperBoundOnErrorProbability (float): select an index i to be unfrozen if the total-variation K(U_i|U^{i-1}) \leq epsilon/(2N) and the probatility of error Pe(U_i | U^{i-1}, Y^{N-1}) \leq epsilon/(2N), where epsilon is short for upperBoundOnErrorProbability 
+
+        xVectorDistribution (VectorDistribution): in a memoryless setting, this is essentially a vector with a-priori entries for P(X=0) and P(X=1). If this is set to None, then we assume a uniform input distribution, in which case the above criterion for i being unfrozen is simplified to Pe(U_i | U^{i-1}, Y^{N-1}) \leq epsilon/N.
+
+        xyVectorDistribution (VectorDistribution): in a memoryless setting, this is essentially a vector with a-posteriori entries for P(X=0) and P(X=1). That is, entry i contains P(X=0,Y=y_i) and P(X=1,Y=y_i).
+    Returns:
+        frozenSet (set): the set of frozen indices
+    """
+
+    assert( n >= 0 )
+    assert( L > 0 )
+    assert( upperBoundOnErrorProbability > 0 )
+    assert( xyDistribution is not None )
+
+    if xDistribution is not None:
+        xDists = []
+        xDists.append([])
+        xDists[0].append(xDistribution)
+
+        for m in range(1,n+1):
+            xDists.append([])
+            for dist in xDists[m-1]:
+                xDists[m].append(dist.minusTransform().upgrade(L))
+                xDists[m].append(dist.plusTransform().upgrade(L))
+
+    xyDists = []
+    xyDists.append([])
+    xyDists[0].append(xyDistribution)
+
+    for m in range(1,n+1):
+        xyDists.append([])
+        for dist in xyDists[m-1]:
+            xyDists[m].append(dist.minusTransform().degrade(L))
+            xyDists[m].append(dist.plusTransform().degrade(L))
+
+    frozenSet = set()
+
+    N = 1 << n
+
+    # TODO: This is sub-optimal. Change this to a calculation similar to what we do with the genie.
+    # TODO: Actually, factor common code out into a new function
+    if xDistribution is not None:
+        delta =  upperBoundOnErrorProbability / (2 * N)
+        for i in 2 ** n:
+            if xyDists[m][i].errorProb() > delta or xDists[m][i].totalVariation() > delta:
+                frozenSet.add(i)
+
+    else:
+        delta =  upperBoundOnErrorProbability / N
+        for i in range(2 ** n):
+            if xyDists[m][i].errorProb() > delta:
+                frozenSet.add(i)
+
+    return frozenSet
+
